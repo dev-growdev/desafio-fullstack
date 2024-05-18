3d0a6af142721e0bd5e3c05dcbc8c1be23631a1e
albrand <alex_brandizzi@hotmail.com>
2024-05-18 17:08:25 -0300

    Add entity

--- src/entities/mentors.entity.js
+++ src/entities/mentors.entity.js
@@ -1,51 +1,58 @@
 const { Sequelize, DataTypes } = require('sequelize')
 const sequelize = new Sequelize('postgres')
 
-const Mentor = sequelize.define('Mentor', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    unique: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notNull: {
-        msg: 'Please enter a name',
+const Mentor = sequelize.define(
+  'Mentor',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a name',
+        },
       },
     },
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notNull: {
-        msg: 'Please enter an email',
-      },
-      isEmail: {
-        msg: 'Please enter a valid email',
-      },
-    },
-  },
-  cpf: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notNull: {
-        msg: 'Please enter a CPF',
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter an email',
+        },
+        isEmail: {
+          msg: 'Please enter a valid email',
+        },
       },
     },
+    cpf: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a CPF',
+        },
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: null,
+    },
   },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    defaultValue: null,
-  },
-})
+  {
+    freezeTableName: true,
+    tableName: 'mentors',
+  }
+)
 
 module.exports = { Mentor }
